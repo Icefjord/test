ced373b62144d10baa62e1d2dcceb8057ca0e301
Gustav Saarup <gustav.i.saarup@outlook.com>
2025-02-06 14:24:51 +0100

    Random fsharp file

---
+++ delibprac12.fsx
@@ -0,0 +1,34 @@
+type Person(name:string, add:string, phone: int) = 
+    member val name = name with get, set
+    member val address = add with get, set
+    member val phone = phone with get, set
+
+type Customer(a,b,c,num:int, mail:bool) = 
+    inherit Person (a,b,c)
+    member val Number = num with get, set
+    member val mail = mail with get, set
+
+let dave = Customer("Dave", "57 Ave", 23764422, 12, false)
+
+printf "
+%A
+%A
+%A
+%A
+%A
+" dave.name dave.address dave.phone dave.Number dave.mail
+
+
+Spiller
+Kort
+Deck
+Delt
+Viser
+Vinder
+Vundne kort
+stak
+vinder hånden
+
+
+Kort()
+Value
